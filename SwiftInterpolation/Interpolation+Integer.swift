d07b9c5ca6a34280c83768ca73b233c0e9f8eaba
Erica Sadun <erica@ericasadun.com>
2018-12-16 10:30:11 -0700

    Created consistent type formatter approach across date, string, and integer

--- SwiftInterpolation/Interpolation+Integer.swift
+++ SwiftInterpolation/Interpolation+Integer.swift
@@ -4,80 +4,7 @@
  
  */
 
-public extension String.StringInterpolation {
-  /// Represents a single numeric radix
-  enum Radix: Int {
-    /// a binary number (base 2)
-    case binary = 2
-    /// an octal number (base 8)
-    case octal = 8
-    /// a decimal number (base 10)
-    case decimal = 10
-    /// a hex number (base 16)
-    case hex = 16
-    
-    /// Returns a radix's optional prefix
-    var prefix: String {
-      return [.binary: "0b", .octal: "0o", .hex: "0x"][self, default: ""]
-    }
-  }
-  
-  /// Return a string representation of the supplied value using a standard radix.
-  ///
-  /// For example,
-  ///
-  /// ```
-  /// "\(42, radix: .hex)" // 2a
-  /// "\(42, radix: .binary)" // 101010
-  /// "\(42, radix: .octal)" // 52
-  /// "\(0x2a, radix: .decimal)" // 42
-  /// "\(5, radix: .binary, isBytewise: true)" // 00000101
-  /// "\(5, radix: .octal, isBytewise: true)" // 0005
-  /// "\(5, radix: .hex, isBytewise: true)" // 05
-  /// "\(5, radix: .hex, prefix: true, isBytewise: true)" // 0x05
-  /// ```
-  ///
-  /// - Parameters:
-  ///   - value: A binary integer
-  ///   - radix: A standard (decimal, binary, octal, or hex) radix
-  ///   - prefix: Adds an optional prefix (`0b`, `0o`, or `0x`) corresponding to the output radix.
-  ///   - bytewise: creates an entire byte in the output string (8 numbers for binary, 4 for octal, 2 for hex), left padding with zeroes.
-  ///   - width: left pads with zeroes to the supplied width, unless the output string is already at least `width` in size.
-  mutating func appendInterpolation<IntegerValue: BinaryInteger>(
-    _ value: IntegerValue,
-    radix: Radix,
-    prefix: Bool = false,
-    isBytewise bytewise: Bool = false,
-    toWidth width: Int = 0
-    ) {
-    
-    // Values are uppercased, producing `FF` instead of `ff`
-    // String is now uppercased-radix-encoded-string
-    var string = String(value, radix: radix.rawValue).uppercased()
-    
-    // Bytewise strings are padded to 2 for hex, 4 for oct, 8 for binary
-    var width = width
-    if bytewise {
-      width = [Radix.binary: 8, .octal: 4, .hex: 2][radix, default: width]
-    }
-    
-    // Strings are pre-padded with 0 to match target widths
-    // String is now (0)* + uppercased-radix-encoded-string
-    if string.count < width {
-      string = String(repeating: "0", count: max(0, width - string.count)) + string
-    }
-    
-    
-    // Prefixes use lower case, sourced from `String.StringInterpolation.Radix`
-    // String is now (optional-prefix) + uppercased-radix-encoded-string
-    if prefix {
-      string = radix.prefix + string
-    }
-    
-    // Insert the interpolation
-    appendLiteral(string)
-  }
-}
+import Foundation
 
 /// A formatter that converts between binary integer values and their textual representations.
 public class IntegerFormatter {
@@ -98,10 +25,34 @@ public class IntegerFormatter {
     }
   }
   
+  /// A standard (decimal, binary, octal, or hex) radix
   public var radix: Radix = .decimal
+  
+  /// Adds an optional prefix (`0b`, `0o`, or `0x`) corresponding to the output radix.
   public var usesPrefix: Bool = false
+  
+  /// Creates an entire byte in the output string (8 numbers for binary, 4 for octal, 2 for hex), left padding with zeroes.
   public var isBytewise: Bool = false
+  
+  /// The minimum width of the output string, which is left padded with zeroes
+  /// unless the output string is already at least `width` in size.
   public var width: Int = 0
+  
+  /// Initializes a new instance of an IntegerFormatter
+  public init(radix: Radix = .decimal,
+              usesPrefix: Bool = false,
+              isBytewise: Bool = false,
+              width: Int = 0) {
+    (self.radix, self.usesPrefix, self.isBytewise, self.width) = (radix, usesPrefix, isBytewise, width)
+  }
+  
+  static func format(radix: Radix = .decimal,
+                     usesPrefix: Bool = false,
+                     isBytewise: Bool = false,
+                     width: Int = 0
+    ) -> IntegerFormatter {
+    return IntegerFormatter(radix: radix, usesPrefix: usesPrefix, isBytewise: isBytewise, width: width)
+  }
   
   /// Returns a string representing the integer value using the
   /// formatter's current settings.
@@ -136,13 +87,20 @@ public class IntegerFormatter {
 }
 
 public extension String.StringInterpolation {
-  /// Interpolates a binary integer value using the supplied integer formatter
+  /// Interpolates a binary integer value using the supplied integer formatter,
+  /// for example:
+  ///
+  /// ```
+  /// "\(15, .format(radix: .hex))" // F
+  /// "\(15, .format(radix: .hex, isBytewise: true))" // 0F
+  /// "\(15, .format(radix: .hex, usesPrefix: true, isBytewise: true))" // 0x0F
+  /// ```
   ///
   /// - Parameters:
   ///   - value: an integer value
   ///   - formatter: a configured integer formatter
   mutating func appendInterpolation<IntegerValue: BinaryInteger>(
-    _ value: IntegerValue, formatter formatter: IntegerFormatter) {
+    _ value: IntegerValue, _ formatter: IntegerFormatter) {
     appendLiteral(formatter.string(from: value))
   }
 }
--- SwiftInterpolation/Interpolation+Padding.swift
+++ SwiftInterpolation/Interpolation+Padding.swift
@@ -1,3 +1,9 @@
+/*
+ 
+ Value interpolation with optional string padding
+ 
+ */
+
 import Foundation
 
 /// A formatter that adjusts string layout.
@@ -6,9 +12,29 @@ public class StringFormatter {
   /// padded with repeated characters.
   public enum TextAlignment { case left, right, center }
   
+  /// The direction that aligns the text
   public var alignment: TextAlignment = .right
+  
+  /// The character used to pad the text
   public var paddingCharacter: Character = " "
+  
+  /// The minimum width of the resulting string
   public var width: Int = 0
+  
+  public init(alignment: TextAlignment = .right,
+              paddingCharacter: Character = " ",
+              width: Int = 0
+    ) {
+    (self.alignment, self.paddingCharacter, self.width) = (alignment, paddingCharacter, width)
+  }
+  
+  static func format(
+    alignment: TextAlignment = .right,
+    paddingCharacter: Character = " ",
+    width: Int = 0
+    ) -> StringFormatter {
+    return StringFormatter(alignment: alignment, paddingCharacter: paddingCharacter, width: width)
+  }
   
   func string(from string: StringLiteralType) -> StringLiteralType {
     
@@ -39,91 +65,15 @@ public extension String.StringInterpolation {
   /// Interpolates using a custom string formatter, allowing the results to
   /// be padded as desired, for example:
   ///
-  /// `"\(23, formatter: formatter, width: 15)"`
+  /// ```
+  /// "\(23, .format(width: 5))" // "   23"
+  /// "\(23, .format(alignment: .left, width: 5))" // "23   "
   ///
   /// - Parameters:
   ///   - value: a value to present
   ///   - formatter: a string formatter
-  mutating func appendInterpolation<Value>(_ value:  Value, formatter: StringFormatter, width: Int = 0) {
+  mutating func appendInterpolation<Value>(_ value:  Value, _ formatter: StringFormatter, width: Int = 0) {
     if width != 0 { formatter.width = width }
     appendLiteral(formatter.string(from: "\(value)"))
-  }
-}
-
-public extension String.StringInterpolation {
-  /// The direction from which a string floats when
-  /// padded with repeated characters.
-  enum TextAlignment { case left, right, center }
-  
-  /// Returns a new string repeatedly padded with a character on
-  /// either or both sides of the source string.
-  ///
-  /// - Returns: A padded string
-  ///
-  /// - Parameters:
-  ///   - string: a string literal to pad
-  ///   - alignment: the direction where the padding pushes the string
-  ///   - character: the character to pad with (defaults to `" "`)
-  ///   - targetWidth: the minimum string width result
-  private func pad(
-    string: StringLiteralType,
-    alignment: TextAlignment,
-    with character: Character = " ",
-    toWidth targetWidth: Int = 0
-    ) -> StringLiteralType {
-    
-    var width = targetWidth
-    
-    // Default the padding to one pad on each side
-    if targetWidth == 0 {
-      switch alignment {
-      case .left, .right: width = string.count + 1
-      case .center: width = string.count + 2
-      }
-    }
-    
-    // Determine core padding required
-    guard width > string.count else { return string }
-    let corePadCount = max(width - string.count, 0)
-    
-    func padding(_ count: Int) -> String {
-      return String(repeating: character, count: count)
-    }
-    
-    switch alignment {
-    case .right:
-      // Left pad "abc" to width 5 with " " -> "  abc"
-      return padding(corePadCount) + string
-    case .left:
-      // Right pad "abc" to width 5 with " " -> "abc  "
-      return string + padding(corePadCount)
-    case .center:
-      // Center pad "abc" to width 5 with " " -> " abc "
-      let halfPad = corePadCount / 2
-      return  padding(corePadCount - halfPad) + string + padding(halfPad)
-    }
-  }
-  
-  /// Interpolates character-padded version of a string literal
-  ///
-  /// ```
-  /// "\("hi", width: 5)" // "   hi"
-  /// "\("hi", width: 5, withPad: ".")" // "...hi"
-  /// "\("hi", width: 5, align: .center)" // "  hi "
-  /// "\("hi", width: 5, align: .right, withPad: ".")" // "hi..."
-  /// ```
-  ///
-  /// - Parameters:
-  ///   - value: a value to interpolate
-  ///   - width: the minimum width for the interpolated value to occupy
-  ///   - alignment: the alignment of the value
-  ///   - padding: the padding character (defaults to `" "`)
-  mutating func appendInterpolation<Value>(
-    _ value:  Value,
-    width: Int,
-    align alignment: String.StringInterpolation.TextAlignment = .right,
-    withPad padding: Character = " "
-    ) {
-    appendLiteral(pad(string: "\(value)", alignment: alignment, with: padding, toWidth: width))
   }
 }
